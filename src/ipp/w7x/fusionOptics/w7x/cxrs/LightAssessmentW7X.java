122904c19e387a67d5b5d33af6717747f82ccf90
oliford <oliford>
2019-06-21 09:08:44 +0000

    Remaining changes from stupid broken crappy maven

--- src/ipp/w7x/fusionOptics/w7x/cxrs/LightAssessmentW7X.java
+++ src/ipp/w7x/fusionOptics/w7x/cxrs/LightAssessmentW7X.java
@@ -117,7 +117,7 @@ public class LightAssessmentW7X {
 	
 	public static boolean writeSolidAngeInfo = true;
 	public static String writeWRLForDesigner = null;//"-20160826";
-	public final static int nAttempts = 5000;
+	public final static int nAttempts = 500000;
 	//*/
 	
 	public static double wavelength = sys.designWavelenth;
