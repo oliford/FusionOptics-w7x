9f17244267f90b002a46f8791f0b03bc4c096145
Oliver Ford  @TacticalGrace <codes20@oliford.co.uk>
2026-03-04 17:34:00 +0100

    cfg

--- src/ipp/w7x/fusionOptics/w7x/cxrs/BackgroundTargetting.java
+++ src/ipp/w7x/fusionOptics/w7x/cxrs/BackgroundTargetting.java
@@ -75,7 +75,7 @@ public class BackgroundTargetting {
 	//public static BeamEmissSpecAEA21 sys = new BeamEmissSpecAEA21(Subsystem.CXRS);
 	//public static BeamEmissSpecAEA21 sys = new BeamEmissSpecAEA21(Subsystem.SMSE);
 	//public static BeamEmissSpecAET21_HST_TwoFlatAndLenses2_BK7 sys = new BeamEmissSpecAET21_HST_TwoFlatAndLenses2_BK7(false, false, Focus.BeamDump);
-	public static BeamEmissSpecAET21_OP2_OneSmallFlatMirror2_BK7 sys = new BeamEmissSpecAET21_OP2_OneSmallFlatMirror2_BK7(Port.AET20, CoordStateT2x.CAD);	
+	public static BeamEmissSpecAET21_OP2_OneSmallFlatMirror2_BK7 sys = new BeamEmissSpecAET21_OP2_OneSmallFlatMirror2_BK7(Port.AET20, CoordStateT2x.measured_AET20_MP24);	
 	//public static BeamEmissSpecAEM21_OP2 sys = new BeamEmissSpecAEM21_OP2(CoordState.measuredOP23);
 	public static SimpleBeamGeometry beams = W7xNBI.def();
 	
